124e37c0f45acaf445f687803a301b947bcacc02
Mikolaj Figurski <meek.f80@gmail.com>
2020-11-03 14:25:40 -0500

    Write proper survey generation

--- functions/src/utils/generate-survey.js
+++ functions/src/utils/generate-survey.js
@@ -1,21 +1,20 @@
 module.exports = (criteria) => {
-    criteria = splitCriteriaType(criteria)
-
-
-
-    return criteria
-}
-
-function splitCriteriaType(criteria) {
-    let incMatch = criteria.matchAll(/(?:(?:inclusions?)?\s*criteria:)((?:\n|.)*?)(?:(?:exclusions?)?\s*criteria:)/gi).next().value
+    let incMatch = criteria.matchAll(/(?:inclusions?\s*criteria:)((?:\n|.)*?)(?:exclusions?\s*criteria:)/gi).next().value
     let excMatch = criteria.matchAll(/(?:exclusions? criteria:)((?:\n|.)*?)$/gi).next().value
 
+
+
     return {
-        inclusion: incMatch ? incMatch[1].trim().split('\n') : [],
-        exclusion: excMatch ? excMatch[1].trim().split('\n') : [],
+        inclusion: incMatch ? makeCriteria(incMatch[1]) : [],
+        exclusion: excMatch ? makeCriteria(excMatch[1]) : [],
     }
 }
 
-function regexMatch(text, reg) {
+function makeCriteria(s) {
+    s = s.replace(/(\n.*:\n(?:\n|.)*)(?:\n\n)/gi, (match, $1) => {
+        let sp = $1.split('\n').filter(i => i.trim() !== '')
+        return sp[0] + ' ' + sp.slice(1).join(', ') + '\n'
+    })
 
+    return s.split('\n').filter(i => i.trim() !== '')
 }
--- functions/src/utils/generate-survey.test.js
+++ functions/src/utils/generate-survey.test.js
@@ -8,22 +8,9 @@ describe('generate-survey', () => {
         expect(res).toStrictEqual({ inclusion: ['i1', 'i2'], exclusion: ['e1', 'e2'] })
     })
 
-    it('accepts shorthand criteria markings', () => {
-        let res = func(`Inclusion:\ni1\nExclusion:\ne1`)
-        expect(res).toStrictEqual({ inclusion: ['i1'], exclusion: ['e1'] })
-    })
-
-    it('accepts missing exclusion criteria', () => {
-        let res = func(`Inclusion:\ni1`)
-        expect(res).toStrictEqual({ inclusion: ['i1'], exclusion: [] })
-
-        res = func(`Criteria:\ni1`)
-        expect(res).toStrictEqual({ inclusion: ['i1'], exclusion: [] })
-    })
-
     it('combines criteria with subpoints', () => {
-        let res = func(`Inclusion:\nSymptoms:\nSymptom1\nSympton2\n\ni2\nExclusion:\ne1\ne2`)
+        let res = func(`Inclusion Criteria:\nSymptoms:\nSymptom1\nSymptom2\n\ni2\nExclusion Criteria:\ne1\ne2`)
 
-        expect(res).toStrictEqual({ inclusion: ['Symptions: Symptom1, Symptom2', 'i2'], exclusion: ['e1', 'e2'] })
+        expect(res).toStrictEqual({ inclusion: ['Symptoms: Symptom1, Symptom2', 'i2'], exclusion: ['e1', 'e2'] })
     })
 })
