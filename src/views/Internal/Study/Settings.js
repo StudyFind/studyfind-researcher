6e04283f2f76a9a5127388d886bf892b0e3aba9e
Mikolaj Figurski <meek.f80@gmail.com>
2020-11-11 17:46:32 -0500

    Init study-settings delete

--- src/views/Internal/Study/Settings.js
+++ src/views/Internal/Study/Settings.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
+import {
+  Heading, Text, Box, Button, Flex,
+  Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter,
+  FormControl, Input,
+  useDisclosure
+} from "@chakra-ui/core";
+
 
 function Settings({ study }) {
-  return <div>Settings</div>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const deleteInput = useRef()
+
+  const onDelete = e => {
+    console.log(deleteInput.current.value)
+    if (deleteInput.current.value != study.nctID) {
+      // TODO: Error out
+      return
+    }
+    // TODO: delete
+    onClose()
+  }
+
+  return (
+    <Box borderWidth="1px" rounded="md" overflow="hidden" bg="white" p="20px">
+      <Text fontSize="sm" color="gray.400">
+        {study.nctID}
+      </Text>
+      <Flex align="center">
+        <Button variantColor="red" onClick={onOpen} >Delete</Button>
+        <Heading size="sm" pl="4">Delete this study:</Heading>
+      </Flex>
+
+      {/* MODAL */}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Delete Study</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <Text>This is a permanent action. Please re-enter the NCT-ID of the study you want to delete.</Text>
+            <FormControl pt={5}>
+              <Input placeholder="Type here..." ref={deleteInput} />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button variantColor="red" onClick={onDelete}>Delete</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  )
 }
 
 export default Settings;
