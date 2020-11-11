999e2cfab04e347b4f8a1a70aed83d0543ab7c08
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-11-11 08:00:35 +0530

    Split Studies View

--- src/views/Internal/Studies/Studies.js
+++ src/views/Internal/Studies/Studies.js
@@ -1,13 +1,7 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import { auth, firestore } from "database/firebase";
 
-import NoStudies from "./NoStudies";
-import StudyCardSmall from "views/Internal/StudyCardSmall";
-
-import { Link } from "react-router-dom";
-import { Heading, Button, Spinner } from "@chakra-ui/core";
-import { FaPlusCircle } from "react-icons/fa";
+import StudiesView from "./StudiesView";
 
 function Studies() {
   const [studies, setStudies] = useState([]);
@@ -33,61 +27,7 @@ function Studies() {
     fetchStudies();
   }, []);
 
-  const GRID = studies.length ? (
-    <>
-      <Head>
-        <Heading>Your Studies</Heading>
-        <Link to="/create">
-          <Button leftIcon={FaPlusCircle} variantColor="teal">
-            Create Study
-          </Button>
-        </Link>
-      </Head>
-      <StudyGrid n={studies.length}>
-        {studies.map((study, index) => (
-          <StudyCardSmall key={index} study={study} />
-        ))}
-      </StudyGrid>
-    </>
-  ) : (
-    <NoStudies />
-  );
-
-  const LOAD = (
-    <PageLoader>
-      <Spinner thickness="4px" speed="0.5s" emptyColor="gray.200" color="teal.500" size="lg" />
-    </PageLoader>
-  );
-
-  return <Page>{loading ? LOAD : GRID}</Page>;
+  return <StudiesView studies={studies} loading={loading} />;
 }
-
-const Page = styled.div`
-  padding: 30px;
-  height: 100%;
-  background: #f8f9fa;
-`;
-
-const Head = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 25px;
-`;
-
-const StudyGrid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-template-rows: ${(props) => "270px".repeat(Math.floor(props.n / 2))};
-  grid-gap: 25px;
-  align-items: flex-start;
-`;
-
-const PageLoader = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100%;
-`;
 
 export default Studies;
