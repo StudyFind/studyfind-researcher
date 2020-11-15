5b3eb331712164838315396b2a61d6ecbcf4b18c
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-11-15 15:45:58 +0530

    Modified Study.js to use Chakra Tabs

--- src/views/Internal/Study/Study.js
+++ src/views/Internal/Study/Study.js
@@ -3,16 +3,15 @@ import styled from "styled-components";
 import { useParams } from "react-router-dom";
 import { fetchStudy } from "database/studies";
 
-import { Spinner, PseudoBox } from "@chakra-ui/core";
+import { Spinner, Tabs, TabList, Tab, TabPanels, TabPanel } from "@chakra-ui/core";
 
 import Details from "./Details";
-import Survey from "./Survey";
+import Survey from "./Survey/Survey";
 import Participants from "./Participants";
 import Settings from "./Settings";
 
 function Study() {
   const { id } = useParams();
-  const [tab, setTab] = useState("Details");
   const [study, setStudy] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -23,35 +22,6 @@ function Study() {
       .finally(() => setLoading(false));
   }, []);
 
-  const render = {
-    Details: <Details study={study} />,
-    Survey: <Survey study={study} />,
-    Participants: <Participants study={study} />,
-    Settings: <Settings study={study} />,
-  };
-
-  const tabs = ["Details", "Survey", "Participants", "Settings"];
-  const TABS = (
-    <Tabs>
-      {tabs.map((t, i) => (
-        <PseudoBox
-          key={i}
-          as="button"
-          color={tab === t ? "white" : "gray.500"}
-          bg={tab === t ? "teal.500" : "transparent"}
-          fontWeight="semibold"
-          py={2}
-          px={4}
-          rounded="md"
-          onClick={() => setTab(t)}
-          _hover={{ bg: tab === t || "gray.200" }}
-        >
-          {t}
-        </PseudoBox>
-      ))}
-    </Tabs>
-  );
-
   const LOAD = (
     <PageLoader>
       <Spinner thickness="4px" speed="0.5s" emptyColor="gray.200" color="teal.500" size="lg" />
@@ -60,8 +30,29 @@ function Study() {
 
   const BODY = (
     <div>
-      {TABS}
-      {render[tab]}
+      <Tabs variantColor="teal">
+        <TabList mb="15px">
+          <TabItem>Details</TabItem>
+          <TabItem>Survey</TabItem>
+          <TabItem>Participants</TabItem>
+          <TabItem>Settings</TabItem>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <Details study={study} />
+          </TabPanel>
+          <TabPanel>
+            <Survey study={study} setStudy={setStudy} />
+          </TabPanel>
+          <TabPanel>
+            <Participants study={study} />
+          </TabPanel>
+          <TabPanel>
+            <Settings study={study} />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 
@@ -74,10 +65,9 @@ const Page = styled.div`
   background: #f8f9fa;
 `;
 
-const Tabs = styled.div`
-  display: flex;
-  grid-gap: 10px;
-  margin-bottom: 10px;
+const TabItem = styled(Tab)`
+  font-weight: 600;
+  color: rgb(161, 175, 192);
 `;
 
 const PageLoader = styled.div`
