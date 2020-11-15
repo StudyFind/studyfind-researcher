3c133898be3873c2f598642818d31f21635aa80c
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-11-15 15:45:38 +0530

    Added heading to details tab

--- src/views/Internal/Study/Details.js
+++ src/views/Internal/Study/Details.js
@@ -1,9 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
+import { Heading, Button } from "@chakra-ui/core";
 import StudyCardLarge from "views/Internal/StudyCardLarge";
 
 function Details({ study }) {
-  return <StudyCardLarge study={study} />;
+  return (
+    <>
+      <Head>
+        <Heading fontSize="30px">Study Details</Heading>
+        <Button variantColor="blue">Edit Details</Button>
+      </Head>
+      <StudyCardLarge study={study} />
+    </>
+  );
 }
+
+const Head = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+`;
 
 export default Details;
