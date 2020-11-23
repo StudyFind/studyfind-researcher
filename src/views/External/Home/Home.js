f0dd6b90600dbdc12ebbff716bd7e7fa63051b66
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-11-23 23:00:33 +0530

    Refactored Home Page

--- src/views/External/Home/Home.js
+++ src/views/External/Home/Home.js
@@ -6,19 +6,12 @@ import Footer from "views/External/Footer";
 import Hero from "./Hero/Hero";
 import Features from "./Features/Features";
 import Team from "./Team/Team";
-import Pricing from "./Pricing/Pricing";
-import Contact from "./Contact/Contact";
 
 function Home() {
   return (
     <div>
       <Header />
       <Hero />
-      <Features />
-      <Team />
-      <Pricing />
-      <Contact />
-      <Footer />
     </div>
   );
 }
