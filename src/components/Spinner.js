dce966b807fb8ca7cf01adb5075d35eae9e5d8f6
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-03 19:21:13 +0530

    Updated Spinner component to fill parent

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,8 +1,20 @@
 import React from "react";
+import styled from "styled-components";
 import { Spinner } from "@chakra-ui/react";
 
 function Loader() {
-  return <Spinner thickness="4px" speed="0.5s" emptyColor="gray.200" color="blue.500" size="lg" />;
+  return (
+    <PageLoader>
+      <Spinner thickness="4px" speed="0.5s" emptyColor="gray.200" color="blue.500" size="lg" />
+    </PageLoader>
+  );
 }
+
+const PageLoader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+`;
 
 export default Loader;
