e4fb053bf10dd62dc1f39ba2bfaeaaea98d71799
Mikolaj Figurski <meek.f80@gmail.com>
2020-12-04 13:28:16 -0500

    Fix switch-function regex

--- functions/src/switch-func.js
+++ functions/src/switch-func.js
@@ -1,10 +1,12 @@
 // `functions/src/switch-func.js`
 
+const { logger } = require("firebase-functions");
+
 // import the switch list of cloud functions
 const funcs = require('./utils/switch-list');
 // note, the const below needs to match exported
 // func name in `/functions/index.js` for switch to work
-const thisFuncNameAsRegex = /studies\/(.*)\/?/;
+const thisFuncNameAsRegex = /studies\/(\w*)/;
 
 // Take the request url, figure out which 'subfunction' user is trying to hit,
 // and pas the req, res on to it. Query like: `/studies/<targetFunc>`
@@ -22,6 +24,8 @@ module.exports = (ctx) => {
         if (match === null) return res.json({ error: "please specify a function to call" });
         const name = match[1];
         if (!funcs[name]) return res.json({ error: `function '${name}' does not exist` });
+
+        logger.log(`running function '${name}'`);
 
         return funcs[name](req, res);
     }
