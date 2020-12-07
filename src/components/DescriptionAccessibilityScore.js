70628306b2aa75922f97cd93663c33d0fc2e5804
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-07 22:20:17 +0530

    Added color to correspond to accessibility score

--- src/components/DescriptionAccessibilityScore.js
+++ src/components/DescriptionAccessibilityScore.js
@@ -1,21 +1,33 @@
 import React from "react";
 import { compute } from "functions";
 
-import { Heading, Text, Progress, Tag, Flex, Tooltip, Icon } from "components";
+import { Heading, Text, Progress, Tag, Flex, Tooltip, Icon, Box } from "components";
 import { FaInfoCircle } from "react-icons/fa";
 
 function DescriptionAccessibilityScore({ description }) {
+  const getColor = (score) => {
+    if (score <= 20) {
+      return "red";
+    } else if (score >= 50) {
+      return "green";
+    } else {
+      return "yellow";
+    }
+  };
+
   const score = compute.readabilityIndex(description);
+  const color = getColor(score);
+
   return (
-    <div>
+    <Box bg={`${color}.100`} p="16px 20px" rounded="md">
       <Flex align="center">
         <Heading mr="8px" size="md">
           Description Accessibility Score
         </Heading>
-        <Tag mr="8px" fontWeight="500" colorScheme="green">
+        <Tag variant="solid" mr="8px" fontWeight="500" colorScheme={color}>
           {score}%
         </Tag>
-        <Tooltip label="This score uses the Flesch–Kincaid readability index">
+        <Tooltip label="This score is derived from the Flesch–Kincaid readability index">
           <Flex align="center">
             <Icon color="gray.500" as={FaInfoCircle} />
           </Flex>
@@ -24,8 +36,8 @@ function DescriptionAccessibilityScore({ description }) {
       <Text color="gray.500">
         Accessible study descriptions are shown to improve participant interest and enrollment rates
       </Text>
-      <Progress value={score} size="md" mt="8px" colorScheme="green" />
-    </div>
+      <Progress value={score} bg="white" size="md" mt="8px" colorScheme={color} />
+    </Box>
   );
 }
 
