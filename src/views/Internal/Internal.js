5008cbc0a4c954c2b904727bd711b797489fc373
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-16 01:55:02 +0530

    Removed Screen page from Internal

--- src/views/Internal/Internal.js
+++ src/views/Internal/Internal.js
@@ -9,7 +9,6 @@ import Settings from "views/Internal/Settings/Settings";
 import Dashboard from "views/Internal/Dashboard/Dashboard";
 import Study from "views/Internal/Study/Study";
 import Notifications from "./Notifications/Notifications";
-import Screen from "./Study/Participants/Screen/Screen";
 import Account from "views/Internal/Account/Account";
 
 function Internal() {
@@ -24,7 +23,6 @@ function Internal() {
           <Route exact path="/create" component={CreateStudy} />
           <Route exact path="/notifications" component={Notifications} />
           <Route exact path="/settings" component={Settings} />
-          <Route exact path="/study/:id/participant/:participantid" component={Screen} />
           <Route exact path="/account" component={Account} />
           <Redirect to="/" />
         </Switch>
