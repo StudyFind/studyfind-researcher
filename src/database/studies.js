07bf51d61e14202a847e8afa49976acfa81b52a7
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-25 01:21:57 +0530

    Querying user and studies from Internal.js

--- src/database/studies.js
+++ src/database/studies.js
@@ -1,12 +1,6 @@
 import { auth, firestore } from "./firebase";
 import axios from "axios";
 
-const map = (snapshot) => {
-  const collection = [];
-  snapshot.forEach((doc) => collection.push({ id: doc.id, ...doc.data() }));
-  return collection;
-};
-
 const makeStudy = (nctID) => {
   return auth.currentUser
     .getIdToken(false)
@@ -29,37 +23,7 @@ const resetStudy = (nctID) => {
     .then(({ data }) => data);
 };
 
-const fetchStudy = async (nctID) => {
-  const document = await firestore.collection("studies").doc(nctID).get();
-  return { id: document.id, ...document.data() };
-};
-
-const fetchStudies = async () => {
-  const { currentUser } = await auth;
-  const snapshot = await firestore
-    .collection("studies")
-    .where("researcher.id", "==", currentUser.uid)
-    .where("published", "==", true)
-    .orderBy("updatedAt", "desc")
-    .get();
-
-  return map(snapshot);
-};
-
-const fetchStudiesWhere = async (field, relation, value) => {
-  const snapshot = await firestore.collection("studies").where(field, relation, value).get();
-  return map(snapshot);
-};
-
 const updateStudy = (study) => firestore.collection("studies").doc(study.id).update(study);
 const deleteStudy = (nctID) => firestore.collection("studies").doc(nctID).delete();
 
-export {
-  makeStudy,
-  resetStudy,
-  fetchStudy,
-  fetchStudies,
-  fetchStudiesWhere,
-  updateStudy,
-  deleteStudy,
-};
+export { makeStudy, resetStudy, updateStudy, deleteStudy };
--- src/views/Internal/Internal.js
+++ src/views/Internal/Internal.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { Box, Flex } from "@chakra-ui/react";
+import { Page } from "components";
+
+import { auth, firestore } from "database/firebase";
+import { useDocument, useCollection } from "hooks";
 
 import Sidebar from "./Sidebar";
 
@@ -11,19 +15,39 @@ import ViewStudy from "views/Internal/Study/ViewStudy/ViewStudy";
 import Notifications from "views/Internal/Notifications/Notifications";
 
 function Internal() {
+  const { uid } = auth.currentUser;
+  const [user] = useDocument(firestore.collection("researchers").doc(uid));
+  const [studies, loading, error] = useCollection(
+    firestore
+      .collection("studies")
+      .where("researcher.id", "==", uid)
+      .where("published", "==", true)
+      .orderBy("updatedAt", "desc")
+  );
+
+  console.log({ studies, loading, error });
+
   return (
     <Flex bg="#f8f9fa">
       <Sidebar />
       <Box ml="280px" w="100%" minH="100vh">
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/study/:nctID" component={ViewStudy} />
-          <Route exact path="/create" component={CreateStudy} />
-          <Route exact path="/notifications" component={Notifications} />
-          <Route exact path="/settings" component={Settings} />
-          <Redirect to="/" />
-        </Switch>
+        <Page isLoading={!studies || error || loading}>
+          <Switch>
+            <Route exact path="/">
+              <Dashboard studies={studies} loading={loading} />
+            </Route>
+            <Route exact path="/dashboard">
+              <Dashboard studies={studies} loading={loading} />
+            </Route>
+            <Route exact path="/study/:nctID">
+              <ViewStudy studies={studies} />
+            </Route>
+            <Route exact path="/create" component={CreateStudy} />
+            <Route exact path="/notifications" component={Notifications} />
+            <Route exact path="/settings" component={Settings} />
+            <Redirect to="/" />
+          </Switch>
+        </Page>
       </Box>
     </Flex>
   );
