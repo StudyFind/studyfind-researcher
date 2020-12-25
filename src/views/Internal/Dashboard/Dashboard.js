382301d4b54ccd5c21377fa1b4f9a14c9c89d0bf
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-25 22:06:15 +0530

    Minor change

--- src/views/Internal/Dashboard/Dashboard.js
+++ src/views/Internal/Dashboard/Dashboard.js
@@ -3,8 +3,8 @@ import React from "react";
 import DashboardGrid from "views/Internal/Dashboard/DashboardGrid";
 import DashboardEmpty from "views/Internal/Dashboard/DashboardEmpty";
 
-function Dashboard({ studies, loading }) {
-  return studies && studies.length ? <DashboardGrid studies={studies} /> : <DashboardEmpty />;
+function Dashboard({ studies }) {
+  return studies.length ? <DashboardGrid studies={studies} /> : <DashboardEmpty />;
 }
 
 export default Dashboard;
