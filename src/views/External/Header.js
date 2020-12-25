55e95339c5cc03c38ee9e742ecb8a3e10ee63d79
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-25 15:38:31 +0530

    Removed External Header Active State

--- src/views/External/Header.js
+++ src/views/External/Header.js
@@ -1,23 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import { NavHashLink as HashLink } from "react-router-hash-link";
-import $ from "jquery";
-
 import { Heading } from "@chakra-ui/react";
-
 import SFLogo from "images/logo.png";
 
 function Header() {
-  const [active, setActive] = useState(false);
-  const LIMIT = 50;
-
-  $(document).scroll(() => {
-    const scroll = $(document).scrollTop();
-    setActive(scroll >= LIMIT);
-  });
-
   return (
-    <Box active={active}>
+    <Box>
       <Logo to="/#">
         <Icon src={SFLogo} />
         <Name fontSize="1.7rem">StudyFind</Name>
@@ -29,29 +18,8 @@ function Header() {
 const Box = styled.div`
   position: fixed;
   top: 0;
-  z-index: 100;
-  background: transparent;
   padding: 50px;
   width: 100vw;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  @media only screen and (max-width: 600px) {
-    padding: 30px;
-  }
-
-  ${(props) =>
-    props.active &&
-    `
-    padding: 24px 50px;
-    background: white;
-    box-shadow: 0 0 10px 0 rgb(240,240,240) !important;
-
-    @media only screen and (max-width: 600px) {
-      padding: 24px 30px;
-    }
-  `}
 `;
 
 const Logo = styled(HashLink)`
