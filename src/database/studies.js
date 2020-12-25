b41b349249a73bef411f2fe744ce86b2695c0687
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-25 22:34:11 +0530

    Refactored Fetch

--- src/database/studies.js
+++ src/database/studies.js
@@ -1,16 +1,25 @@
 import { auth, firestore } from "./firebase";
 import axios from "axios";
 
-const makeStudy = (nctID) => {
-  return auth.currentUser
-    .getIdToken(false)
-    .then((idToken) =>
-      axios.get("https://us-central1-studyfind-researcher.cloudfunctions.net/studies/makeStudy", {
-        params: { nctID, idToken },
-      })
-    )
-    .then(({ data }) => data);
-};
+const makeStudy = (nctID) =>
+  new Promise((resolve, reject) => {
+    return auth.currentUser
+      .getIdToken(false)
+      .then((idToken) =>
+        axios.get("https://us-central1-studyfind-researcher.cloudfunctions.net/studies/makeStudy", {
+          params: { nctID, idToken },
+        })
+      )
+      .then(({ data }) => {
+        const { study, error } = data;
+
+        if (study) {
+          resolve(study);
+        } else {
+          reject(error);
+        }
+      });
+  });
 
 const resetStudy = (nctID) => {
   return auth.currentUser
