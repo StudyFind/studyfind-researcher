d3aa4ae213fc20c2e4ae54b63214dd16cf3d068a
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-25 15:45:52 +0530

    Removed External Header dependency on styled-components

--- src/views/External/Header.js
+++ src/views/External/Header.js
@@ -1,40 +1,19 @@
 import React from "react";
-import styled from "styled-components";
 import { NavHashLink as HashLink } from "react-router-hash-link";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Flex, Box, Image } from "@chakra-ui/react";
 import SFLogo from "images/logo.png";
 
 function Header() {
   return (
-    <Box>
-      <Logo to="/#">
-        <Icon src={SFLogo} />
-        <Name fontSize="1.7rem">StudyFind</Name>
-      </Logo>
+    <Box position="fixed" top="0" p="50px" w="100vw">
+      <HashLink to="/#">
+        <Flex align="center">
+          <Image src={SFLogo} h="2rem" mr="10px" />
+          <Heading fontSize="1.7rem">StudyFind</Heading>
+        </Flex>
+      </HashLink>
     </Box>
   );
 }
-
-const Box = styled.div`
-  position: fixed;
-  top: 0;
-  padding: 50px;
-  width: 100vw;
-`;
-
-const Logo = styled(HashLink)`
-  all: unset;
-  cursor: pointer;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`;
-
-const Icon = styled.img`
-  height: 2rem;
-  margin-right: 10px;
-`;
-
-const Name = styled(Heading)``;
 
 export default Header;
