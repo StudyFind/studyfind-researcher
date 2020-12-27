1f18ba3411a1a84907085f0907a93ae113270249
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-28 00:33:43 +0530

    Combined Screening.js and ScreeningView.js

--- src/views/Internal/Study/CreateStudy/Screening/Screening.js
+++ src/views/Internal/Study/CreateStudy/Screening/Screening.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import ScreeningView from "./ScreeningView";
+import { Heading, Text, Button, Flex, Grid } from "@chakra-ui/react";
+import { FaTrash, FaPlus } from "react-icons/fa";
+import Question from "./Question";
 
 function Screening({ study, setStudy, setTab }) {
   const [questions, setQuestions] = useState(study.questions || []);
@@ -12,7 +14,7 @@ function Screening({ study, setStudy, setTab }) {
 
   const updateQuestion = (index, name, value) => {
     const updated = [...questions];
-    updated[index] = { ...updated[index], [name]: value };
+    updated[index] = { ...questions[index], [name]: value };
     setQuestions(updated);
   };
 
@@ -31,14 +33,46 @@ function Screening({ study, setStudy, setTab }) {
   };
 
   return (
-    <ScreeningView
-      questions={questions}
-      createQuestion={createQuestion}
-      updateQuestion={updateQuestion}
-      deleteQuestion={deleteQuestion}
-      deleteAllQuestions={deleteAllQuestions}
-      handleSubmit={handleSubmit}
-    />
+    <div>
+      <Heading size="lg" mb="10px">
+        Modifying Eligibility Criteria
+      </Heading>
+      <Text mb="10px" color="gray.500">
+        These exclusion and inclusion criteria will be used to automatically generate a screening
+        survey for interested participants to answer in their process of enrolling.
+      </Text>
+      {questions && questions.length ? (
+        <Button
+          leftIcon={<FaTrash />}
+          colorScheme=""
+          color="red.500"
+          _hover={{ bg: "red.100" }}
+          onClick={deleteAllQuestions}
+        >
+          Delete All
+        </Button>
+      ) : null}
+      <Grid w="100%" gap="10px" py="10px">
+        {questions &&
+          questions.map((question, index) => (
+            <Question
+              key={index}
+              index={index}
+              question={question}
+              updateQuestion={updateQuestion}
+              deleteQuestion={deleteQuestion}
+            />
+          ))}
+        <Button leftIcon={<FaPlus />} color="gray.500" onClick={createQuestion}>
+          Add Question
+        </Button>
+      </Grid>
+      <Flex justify="flex-end" gridGap="10px">
+        <Button colorScheme="blue" onClick={handleSubmit} type="submit">
+          Submit
+        </Button>
+      </Flex>
+    </div>
   );
 }
 
