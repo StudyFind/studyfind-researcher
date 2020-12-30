a417b3fc71677c44ba03f015b34ba6839ecc1a61
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-30 12:04:22 +0530

    Combined Auth and AuthView

--- src/views/External/Auth/Auth.js
+++ src/views/External/Auth/Auth.js
@@ -1,5 +1,11 @@
 import React, { useState } from "react";
-import AuthView from "./AuthView";
+import styled from "styled-components";
+import { Box, Flex } from "@chakra-ui/react";
+
+import Login from "./Tabs/Login";
+import Signup from "./Tabs/Signup";
+import ForgotPassword from "./Tabs/ForgotPassword";
+import ResetPassword from "./Tabs/ResetPassword";
 
 function Auth() {
   const getDefaultTab = () => {
@@ -12,7 +18,52 @@ function Auth() {
 
   const [tab, setTab] = useState(getDefaultTab());
 
-  return <AuthView tab={tab} setTab={setTab} />;
+  const render = {
+    login: <Login setTab={setTab} />,
+    signup: <Signup setTab={setTab} />,
+    forgotPassword: <ForgotPassword setTab={setTab} />,
+    resetPassword: <ResetPassword setTab={setTab} />,
+  };
+
+  return (
+    <Flex justify="center" align="center" h="100vh">
+      <Box w="350px" bg="#f8f9fa" borderWidth="1px" borderColor="gray" rounded="md">
+        <Flex borderBottom="1px solid #e7eaf3">
+          <Tab selected={tab === "signup"} onClick={() => setTab("signup")}>
+            Sign up
+          </Tab>
+          <Tab selected={tab === "login"} onClick={() => setTab("login")}>
+            Login
+          </Tab>
+        </Flex>
+        {render[tab]}
+      </Box>
+    </Flex>
+  );
 }
+
+const Tab = styled.button`
+  flex: 1;
+  color: #96a0aa;
+  text-align: center;
+  padding: 1rem 0.75rem;
+  cursor: pointer;
+  font-weight: 600;
+  margin-bottom: -2px;
+  outline: none;
+
+  &:hover,
+  &:focus,
+  &:active {
+    color: rgb(49, 130, 207);
+  }
+
+  ${(props) =>
+    props.selected &&
+    `
+      color: rgb(49, 130, 207);
+      border-bottom: 3px solid rgb(49, 130, 207);
+  `}
+`;
 
 export default Auth;
