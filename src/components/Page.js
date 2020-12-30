e5bdb00f10acecea20a7948b2f1efdfc52d85a25
Yohan Jhaveri <yohanjhaveri@gmail.com>
2020-12-30 12:03:59 +0530

    Remove Page.js dependence on styled-components

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,15 +1,13 @@
 import React from "react";
-import styled from "styled-components";
+import { Box } from "@chakra-ui/react";
 import { Spinner } from "components";
 
 function Page({ children, isLoading }) {
-  return <Box>{isLoading ? <Spinner /> : children}</Box>;
+  return (
+    <Box p="40px" h="100%">
+      {isLoading ? <Spinner /> : children}
+    </Box>
+  );
 }
-
-const Box = styled.div`
-  padding: 40px;
-  height: 100%;
-  background: #f8f9fa;
-`;
 
 export default Page;
