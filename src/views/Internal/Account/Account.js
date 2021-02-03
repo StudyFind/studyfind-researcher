bb7d8966f58bea66fcc0c41b116718483783d9d6
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-03 00:41:49 -0500

    Account Page Enhancements

--- src/views/Internal/Account/Account.js
+++ src/views/Internal/Account/Account.js
@@ -1,39 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 import { signout } from "database";
 import { auth } from "database/firebase";
 
-import { Heading, Text, Button, Box, Grid } from "@chakra-ui/react";
-import { Input } from "components";
+import { Heading, Button, Grid } from "@chakra-ui/react";
+import { Input, Textarea } from "components";
 import { FaDoorOpen } from "react-icons/fa";
 
-import ProfilePicture from "./ProfilePicture";
 import { firestore } from "database/firebase";
 
 function Account({ user }) {
-  const { uid, displayName, email } = auth.currentUser;
-  const profileRef = firestore.collection("researchers").doc(uid);
-  const originalProfileInputs = {
-    name: user.name || "",
-  };
-  const [profileInputs, setProfileInputs] = useState(originalProfileInputs);
-  const [profileErrors, setProfileErrors] = useState({ name: "" });
-  const [inputs, setInputs] = useState({});
+  const { uid, email } = auth.currentUser;
+  const researcherRef = firestore.collection("researchers").doc(uid);
 
-  const handleProfileChange = (name, value) => {
-    setProfileInputs({ ...profileInputs, [name]: value });
+  const original = {
+    name: user.name || "",
+    bio: user.bio || "",
+    organization: user.organization || "",
   };
+
+  const [inputs, setInputs] = useState(original);
+  const [errors, setErrors] = useState({ name: "" });
+
   const handleChange = (name, value) => {
     setInputs({ ...inputs, [name]: value });
   };
 
-  const handleProfileUpdate = () => {
-    setProfileErrors({
-      name: !profileInputs.name,
-    });
-    if (!profileInputs.name) return;
-    profileRef.set(profileInputs);
+  const handleUpdate = () => {
+    setErrors({ name: !inputs.name });
+    if (!inputs.name) return;
+    researcherRef.set({ name: inputs.name, organization: inputs.organization, bio: inputs.bio });
   };
 
   return (
@@ -45,75 +42,35 @@ function Account({ user }) {
         </Button>
       </Head>
       <hr />
-      <Grid templateColumns="300px 1fr" gap="30px" p="30px 0">
-        <Box>
-          <Heading size="md">Profile</Heading>
-          <Text>This section contains all your public profile information</Text>
-        </Box>
-        <Grid gap="15px">
-          <ProfilePicture uid={uid} name={inputs.name} />
-          <Input
-            value={profileInputs.name}
-            name="name"
-            label="name"
-            error={profileErrors.name}
-            onChange={handleProfileChange}
-          />
-          <Input value={email} label="email" readOnly />
-          {/* <Input value={inputs.organization} label="Organization" onChange={handleChange} /> */}
-          {!(
-            JSON.stringify(originalProfileInputs) ===
-            JSON.stringify(profileInputs)
-          ) && (
-            <Button colorScheme="blue" onClick={handleProfileUpdate}>
-              Update Profile
-            </Button>
-          )}
-        </Grid>
-      </Grid>
-      <hr />
-      <Grid templateColumns="300px 1fr" gap="30px" p="30px 0">
-        <Box>
-          <Heading size="md">Change Password</Heading>
-          <Text>
-            This section contains all your profile information as well as your
-            bio and interests
-          </Text>
-        </Box>
-        <Grid gap="15px">
-          <Input
-            value={inputs.oldPassword}
-            label="Old Password"
-            onChange={handleChange}
-          />
-          <Input
-            value={inputs.newPassword}
-            label="New Password"
-            onChange={handleChange}
-          />
-          <Button colorScheme="blue">Change Password</Button>
-        </Grid>
-      </Grid>
-      <hr />
-      <Grid templateColumns="300px 1fr" gap="30px" p="30px 0">
-        <Box>
-          <Heading size="md" color="red.500">
-            Delete Account
-          </Heading>
-          <Text>
-            Deleting your account will delete all your data including any
-            studies you have created.
-          </Text>
-        </Box>
-        <Grid gap="15px">
-          <Input value={inputs.email} label="Email" onChange={handleChange} />
-          <Input
-            value={inputs.password}
-            label="Password"
-            onChange={handleChange}
-          />
-          <Button colorScheme="red">Delete Account</Button>
-        </Grid>
+
+      <Grid gap="20px" p="20px 0" w="400px">
+        <Input value={email} label="Email" readOnly />
+        <Input
+          label="Name"
+          name="name"
+          value={inputs.name}
+          error={errors.name}
+          onChange={handleChange}
+        />
+        <Input
+          label="Organization"
+          name="organization"
+          value={inputs.organization}
+          onChange={handleChange}
+        />
+        <Textarea
+          label="Bio"
+          name="bio"
+          value={inputs.bio}
+          error={errors.bio}
+          onChange={handleChange}
+          height="108px"
+        />
+        {!(JSON.stringify(original) === JSON.stringify(inputs)) && (
+          <Button colorScheme="green" onClick={handleUpdate}>
+            Save Changes
+          </Button>
+        )}
       </Grid>
     </>
   );
