7724d82d07a2c53103ace58071148503aabb974e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-03 00:41:17 -0500

    Updated signup function to accept user name

--- src/views/External/Auth/Blocks/index.js
+++ src/views/External/Auth/Blocks/index.js
@@ -1,5 +1,6 @@
 export { default as Form } from "./Form";
 export { default as Heading } from "./Heading";
+export { default as Name } from "./Name";
 export { default as Email } from "./Email";
 export { default as Password } from "./Password";
 export { default as Button } from "./Button";
