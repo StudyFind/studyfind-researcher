54eb4db5c8891f2cf961fee6d5c86eb5447507ec
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-04 00:15:46 -0500

    Fixed spelling error

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -1,9 +1,9 @@
 import { useState, useEffect } from "react";
-import { useDocument as useFirstoreDocument } from "react-firebase-hooks/firestore";
+import { useDocument as useFirestoreDocument } from "react-firebase-hooks/firestore";
 
 function useDocument(query, options) {
   const [document, setDocument] = useState();
-  const [snapshot, loading, error] = useFirstoreDocument(query, options);
+  const [snapshot, loading, error] = useFirestoreDocument(query, options);
 
   useEffect(() => {
     console.log(snapshot);
