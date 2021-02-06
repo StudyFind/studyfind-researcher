bf973f70f77d9c61076cffa271baeb88e18daa52
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-05 21:20:51 -0500

    Auth Refactoring

--- src/views/External/Auth/Forms/Login.js
+++ src/views/External/Auth/Forms/Login.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import { useAuthForm } from "hooks";
-import { signin } from "database";
+import { signin } from "database/auth";
+
 import { Form, Heading, Email, Password, Button, TabLink } from "views/External/Auth/Blocks";
 
 function Login({ setTab }) {
-  const { inputs, errors, loading, handleInput, handleSubmit } = useAuthForm({
+  const { inputs, errors, loading, handleChange, handleSubmit } = useAuthForm({
     initial: { email: "", password: "" },
     onSubmit: signin,
   });
@@ -13,8 +14,8 @@ function Login({ setTab }) {
   return (
     <Form onSubmit={() => handleSubmit(inputs.email, inputs.password)}>
       <Heading>Welcome Back!</Heading>
-      <Email value={inputs.email} error={errors.email} onChange={handleInput} />
-      <Password value={inputs.password} error={errors.password} onChange={handleInput} />
+      <Email value={inputs.email} error={errors.email} onChange={handleChange} />
+      <Password value={inputs.password} error={errors.password} onChange={handleChange} />
       <Button loading={loading}>Login</Button>
       <TabLink onClick={() => setTab("forgotPassword")}>Forgot Password?</TabLink>
     </Form>
--- src/views/Internal/Settings/DeleteAccount.js
+++ src/views/Internal/Settings/DeleteAccount.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 import { useAuthForm } from "hooks";
-import { deleteUser } from "database";
+import { deleteUser } from "database/auth";
+
 import { Form, Heading, Email, Password, Button } from "views/External/Auth/Blocks";
 import { Box } from "@chakra-ui/react";
 
 function ChangePassword() {
-  const { inputs, errors, loading, handleInput, handleSubmit } = useAuthForm({
+  const { inputs, errors, loading, handleChange, handleSubmit } = useAuthForm({
     initial: { email: "", password: "" },
     onSubmit: deleteUser,
   });
@@ -15,8 +16,8 @@ function ChangePassword() {
     <Box w="350px" bg="white" borderWidth="1px" borderColor="gray" rounded="md">
       <Form onSubmit={() => handleSubmit(inputs.email, inputs.password)}>
         <Heading color="red.500">Delete Account</Heading>
-        <Email value={inputs.email} error={errors.email} onChange={handleInput} />
-        <Password value={inputs.password} error={errors.password} onChange={handleInput} />
+        <Email value={inputs.email} error={errors.email} onChange={handleChange} />
+        <Password value={inputs.password} error={errors.password} onChange={handleChange} />
         <Button loading={loading} colorScheme="red">
           Confirm Delete Account
         </Button>
