0adfee74ab12c99fcb584e26b6cecf270d389403
Ziyao Ren <zren1996813@hotmail.com>
2021-02-14 14:05:03 -0500

    refactor and reformat the button

--- src/views/Internal/Notifications/Notifications.js
+++ src/views/Internal/Notifications/Notifications.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { auth, firestore } from "database/firebase";
 
-import { Heading, Box, Button } from "@chakra-ui/react";
+import { Heading, Box, Button, Flex, Grid } from "@chakra-ui/react";
 import { Loader, Message } from "components";
 import Notification from "./Notification";
 
@@ -10,59 +10,42 @@ function Notifications() {
   const { uid } = auth.currentUser;
   const [notifications, setNotifications] = useState([]);
   const [documents, setDocuments] = useState([]);
-  const [fetchedAll, setFetchedAll] = useState([]);
+  const [fetchedAll, setFetchedAll] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
   const handleFetchAdditional = async () => {
+    let lastDoc;
     if (documents.length && notifications.length) {
-      console.log("Fetched additional round of notifications");
-      console.log(documents);
-      try {
-        const lastDoc = documents[documents.length - 1];
-        const snapshot = await firestore
-          .collection("researchers")
-          .doc(uid)
-          .collection("notifications")
-          .orderBy("time", "desc")
-          .startAfter(lastDoc)
-          .limit(1)
-          .get();
-
-        const collections = [];
-        const docs = [];
-        snapshot.forEach((doc) => collections.push({ id: doc.id, ...doc.data() }));
-        setNotifications((prev) => prev.concat(collections));
-        snapshot.forEach((doc) => docs.push(doc));
-        setDocuments((prev) => prev.concat(docs));
-      } catch (e) {
-        console.log(e);
-        setError(e);
-      }
+      lastDoc = documents[documents.length - 1];
     } else {
-      console.log("Fetched first round of notifications");
-      try {
-        setLoading(true);
-        const snapshot = await firestore
-          .collection("researchers")
-          .doc(uid)
-          .collection("notifications")
-          .orderBy("time", "desc")
-          .limit(1)
-          .get();
-
-        const collections = [];
-        const docs = [];
-        snapshot.forEach((doc) => collections.push({ id: doc.id, ...doc.data() }));
-        setNotifications(collections);
-        snapshot.forEach((doc) => docs.push(doc));
-        setDocuments(docs);
-      } catch (e) {
-        console.log(e);
-        setError(e);
-      }
-      setLoading(false);
+      lastDoc = "";
     }
+    try {
+      let snapshot;
+      snapshot = await firestore
+        .collection("researchers")
+        .doc(uid)
+        .collection("notifications")
+        .orderBy("time", "desc")
+        .startAfter(lastDoc)
+        .limit(10)
+        .get();
+
+      const collections = [];
+      const docs = [];
+      snapshot.forEach((doc) => collections.push({ id: doc.id, ...doc.data() }));
+      setNotifications((prev) => prev.concat(collections));
+      snapshot.forEach((doc) => docs.push(doc));
+      setDocuments((prev) => prev.concat(docs));
+      if (collections.length < 10) {
+        setFetchedAll(true);
+      }
+    } catch (e) {
+      setError(e);
+    }
+
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -77,28 +60,37 @@ function Notifications() {
       <Heading size="lg" mb="25px">
         Notifications
       </Heading>
-      <Box borderWidth="1px" rounded="md" bg="white">
-        {notifications && notifications.length ? (
-          notifications.map((notification, index) => (
-            <Notification key={index} notification={notification} />
-          ))
-        ) : (
-          <Box h="500px">
-            <Message
-              type="neutral"
-              title="Nothing to show"
-              description="You do not have any notifications right now"
-            />
+
+      {notifications && notifications.length ? (
+        <Grid gap="20px">
+          <Box borderWidth="1px" rounded="md" bg="white">
+            {notifications.map((notification, index) => (
+              <Notification key={index} notification={notification} />
+            ))}
           </Box>
-        )}
-      </Box>
-      <Button
-        isLoading={loading}
-        loadingText="Fetching notifications..."
-        onClick={handleFetchAdditional}
-      >
-        Load more notifications
-      </Button>
+          <Flex justify="center" align="center">
+            {fetchedAll ? (
+              <></>
+            ) : (
+              <Button
+                isLoading={loading}
+                loadingText="Fetching notifications..."
+                onClick={handleFetchAdditional}
+              >
+                Load more notifications
+              </Button>
+            )}
+          </Flex>
+        </Grid>
+      ) : (
+        <Box h="500px">
+          <Message
+            type="neutral"
+            title="Nothing to show"
+            description="You do not have any notifications right now"
+          />
+        </Box>
+      )}
     </>
   );
 }
