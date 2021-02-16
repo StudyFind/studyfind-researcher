d884f6ad59e6ae0f9f0d5af06946eedef8188570
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-16 15:44:42 -0500

    - Added published and unpublished tag - Showing published and unpublished studies

--- src/views/Internal/Dashboard/Dashboard.js
+++ src/views/Internal/Dashboard/Dashboard.js
@@ -4,8 +4,8 @@ import DashboardGrid from "views/Internal/Dashboard/DashboardGrid";
 import DashboardEmpty from "views/Internal/Dashboard/DashboardEmpty";
 
 function Dashboard({ studies }) {
-  const published = studies.filter((study) => study.published);
-  return published.length ? <DashboardGrid studies={published} /> : <DashboardEmpty />;
+  // const published = studies.filter((study) => study.published);
+  return studies.length ? <DashboardGrid studies={studies} /> : <DashboardEmpty />;
 }
 
 export default Dashboard;
