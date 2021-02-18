a1ac4987336d791601d5289befc029a958b35e7e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-17 22:47:08 -0500

    Edited description text

--- src/views/Internal/Study/CreateStudy/Eligibility/EligibilityHead.js
+++ src/views/Internal/Study/CreateStudy/Eligibility/EligibilityHead.js
@@ -8,11 +8,12 @@ function EligibilityHead() {
         Modifying Eligibility Criteria
       </Heading>
       <Text mb="10px" color="gray.500">
-        It is important that you modify these inclusion and exclusion criteria into <b>Yes / No</b>
+        It is important that you modify these inclusion and exclusion criteria into{" "}
+        <b>Yes / No / Maybe</b>
         &nbsp;questions. These questions will form a screening survey which will be asked to
-        participants when they are enrolling for your study. Their responses to these questions will
-        be used to calculate their eligiblity score, a metric that should help you determine how
-        likely a participant is to be eligible.
+        participants when they enroll for your study. Their responses to these questions will be
+        used to calculate their eligiblity score, a metric that should help you determine how likely
+        a participant is to be eligible.
       </Text>
     </>
   );
