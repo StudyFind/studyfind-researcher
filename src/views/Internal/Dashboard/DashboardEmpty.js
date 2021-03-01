1e7562c397b4925b04d531b084a30d64730fed11
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-02-28 23:01:59 -0500

    Enhanced welcome feature

--- src/views/Internal/Dashboard/DashboardEmpty.js
+++ src/views/Internal/Dashboard/DashboardEmpty.js
@@ -1,23 +1,34 @@
 import React from "react";
 
 import { Link } from "react-router-dom";
-import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import { Alert, AlertIcon, Box, Flex, Heading, Text, Button } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa";
 
 function DashboardEmpty() {
   return (
-    <Box w="450px">
-      <Heading fontSize="32px">Create your first study</Heading>
-      <Text color="gray.500" mt="8px">
-        You can add your study using its Clinical Trials ID and begin recruiting and managing
-        participants almost instantaneously. StudyFind automates a lot of your work for you.
-      </Text>
-      <Link to="/fetch">
-        <Button mt="40px" leftIcon={<FaPlus />} colorScheme="blue">
-          Create Study
-        </Button>
-      </Link>
-    </Box>
+    <Flex h="100%" direction="column">
+      <Box w="500px">
+        <Heading size="lg">Create your first study</Heading>
+        <Text color="gray.500" mt="8px">
+          You can add your study using its Clinical Trials ID and begin recruiting and managing
+          participants almost instantaneously. StudyFind automates a lot of your work for you.
+        </Text>
+        <Link to="/fetch">
+          <Button mt="40px" leftIcon={<FaPlus />} colorScheme="blue">
+            Create Study
+          </Button>
+        </Link>
+      </Box>
+      <Box mt="auto">
+        <Link to="/welcome">
+          <Alert status="info" rounded="md" color="blue.500" mt="auto">
+            <AlertIcon />
+            Click here to automatically fetch all your studies from clinicaltrials.gov and add them
+            to your StudyFind account.
+          </Alert>
+        </Link>
+      </Box>
+    </Flex>
   );
 }
 
