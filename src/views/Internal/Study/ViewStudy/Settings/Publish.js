46f2d2a5504995f3bb0b80c3aac1d6a3d90ab4ef
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-03-20 13:38:16 -0400

    Redirected Publish button to CreateStudy instead of checking and publishing the study directly

--- src/views/Internal/Study/ViewStudy/Settings/Publish.js
+++ src/views/Internal/Study/ViewStudy/Settings/Publish.js
@@ -1,32 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { Box, Heading, Text, Button } from "@chakra-ui/react";
 
-import { Confirm } from "components";
-import { Box, Heading, Text, Button, FormErrorMessage, useToast } from "@chakra-ui/react";
-import { updateStudy } from "database/studies";
-
-function Update({ study }) {
-  const toast = useToast();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [open, setOpen] = useState(false);
-
-  const handlePublish = () => {
-    setLoading(true);
-    updateStudy(study.id, { ...study, published: true, activated: true })
-      .then(() => {
-        toast({
-          title: "Study Published!",
-          description:
-            "Your study was successfully published is now available for participants to view and enroll",
-          status: "success",
-          duration: 2500,
-          isClosable: true,
-          position: "top",
-        });
-      })
-      .catch(setError)
-      .finally(() => setLoading(false));
-  };
+function Publish({ study }) {
+  const history = useHistory();
 
   return (
     <Box p="20px" borderBottom="1px solid #f1f2f3">
@@ -37,34 +14,11 @@ function Update({ study }) {
         Once your study is published participants will be able to enroll for it; however, you will
         no longer be able to edit the study title, description, and screening survey.
       </Text>
-      <Button
-        type="submit"
-        onClick={() => setOpen(true)}
-        colorScheme="green"
-        isLoading={loading}
-        loadingText="Updating"
-      >
+      <Button colorScheme="green" onClick={() => history.push(`/create/${study.id}/details`)}>
         Publish
       </Button>
-      {error && <FormErrorMessage>{error}</FormErrorMessage>}
-      <Confirm
-        title="Publish Study"
-        buttonText="Publish"
-        loading={loading}
-        loadingText="Publishing"
-        color="green"
-        open={open}
-        setOpen={setOpen}
-        handleConfirm={handlePublish}
-      >
-        Are you sure you want to publish study <b>{study.id}</b>?
-        <br />
-        <br />
-        Once the study is published, you will not be able to edit the study title, description, and
-        screening survey.
-      </Confirm>
     </Box>
   );
 }
 
-export default Update;
+export default Publish;
