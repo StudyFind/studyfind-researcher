4d55356481b5ea112157c08bc24067e19f5f868e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-04-06 19:11:05 -0400

    Lowered state from Screening to ScreeningView and allow editing of published studies

--- src/views/Internal/Study/ViewStudy/Screening/ScreeningEdit.js
+++ src/views/Internal/Study/ViewStudy/Screening/ScreeningEdit.js
@@ -1,22 +1,85 @@
 import React from "react";
 import lodash from "lodash";
-import { Flex, Grid, Heading, Button, Icon, IconButton } from "@chakra-ui/react";
+
+import { useArray } from "hooks";
+import { updateStudy } from "database/studies";
+
+import {
+  Flex,
+  Grid,
+  Heading,
+  Button,
+  Icon,
+  IconButton,
+} from "@chakra-ui/react";
 import { Input, Select } from "components";
-import { SortableContainer, SortableElement, SortableHandle } from "react-sortable-hoc";
+import {
+  SortableContainer,
+  SortableElement,
+  SortableHandle,
+} from "react-sortable-hoc";
 import { FaTrash, FaPlus, FaBars } from "react-icons/fa";
 
-function ScreeningEdit({
-  original,
-  questions,
-  handleCancel,
-  createQuestion,
-  updateQuestion,
-  deleteQuestion,
-  deleteAllQuestions,
-  handleSubmit,
-  setQuestions,
-  errors,
-}) {
+function ScreeningEdit({ study, setEdit }) {
+  const [
+    questions,
+    setQuestions,
+    { appendElement, updateElement, deleteElement, clearArray },
+  ] = useArray(
+    study.questions.map((question) => ({
+      value: question,
+      error: { type: false, prompt: false },
+    }))
+  );
+
+  const deleteQuestion = deleteElement;
+  const deleteAllQuestions = clearArray;
+
+  const createQuestion = () => {
+    appendElement({
+      value: { prompt: "", type: "Inclusion" },
+      error: { type: false, prompt: false },
+    });
+  };
+
+  const updateQuestion = (index, name, value) => {
+    updateElement(
+      {
+        value: { ...questions[index].value, [name]: value },
+        error: { ...questions[index].error, [name]: !value },
+      },
+      index
+    );
+  };
+
+  const handleCancel = () => {
+    setQuestions(
+      study.questions.map((question) => ({
+        value: question,
+        error: { type: false, prompt: false },
+      }))
+    );
+    setEdit(false);
+  };
+
+  const handleSubmit = () => {
+    const updated = questions.map(({ value }) => ({
+      value,
+      error: { type: !value.type, prompt: !value.prompt },
+    }));
+
+    const errors = updated.map((q) => [q.error.type, q.error.prompt]).flat();
+    const invalid = errors.reduce((overall, next) => overall || next);
+
+    if (invalid) {
+      setQuestions(updated);
+      return;
+    }
+
+    updateStudy(study.id, { questions: questions.map((q) => q.value) });
+    setEdit(false);
+  };
+
   const onSortEnd = ({ oldIndex, newIndex }) => {
     const newArray = [...questions];
     const removed = newArray.splice(oldIndex, 1);
@@ -45,12 +108,12 @@ function ScreeningEdit({
           ) : null}
           {!lodash.isEqual(
             questions.map((q) => q.value),
-            original
-          ) ? (
+            study.questions
+          ) && (
             <Button colorScheme="green" onClick={handleSubmit}>
               Save Changes
             </Button>
-          ) : null}
+          )}
         </Flex>
       </Flex>
       <Grid w="100%" gap="10px" py="10px">
@@ -60,7 +123,6 @@ function ScreeningEdit({
           onSortEnd={onSortEnd}
           updateQuestion={updateQuestion}
           deleteQuestion={deleteQuestion}
-          errors={errors}
         />
         <Button leftIcon={<FaPlus />} color="gray.500" onClick={createQuestion}>
           Add Question
@@ -76,48 +138,52 @@ const DragHandle = SortableHandle(() => (
   </Flex>
 ));
 
-const SortableItem = SortableElement(({ value, error, i, updateQuestion, deleteQuestion }) => (
-  <Flex gridGap="10px" w="100%">
-    <DragHandle />
-    <Select
-      w="210px"
-      name="type"
-      value={value.type}
-      error={error.type}
-      onChange={(name, value) => updateQuestion(i, name, value)}
-      options={["Inclusion", "Exclusion"]}
-    />
-    <Input
-      placeholder="Question Prompt"
-      name="prompt"
-      value={value.prompt}
-      error={error.prompt}
-      onChange={(name, value) => updateQuestion(i, name, value)}
-    />
-    <IconButton
-      colorScheme=""
-      color="gray.500"
-      _hover={{ color: "red.500", bg: "red.100" }}
-      icon={<FaTrash />}
-      onClick={() => deleteQuestion(i)}
-    />
-  </Flex>
-));
-
-const SortableList = SortableContainer(({ items, updateQuestion, deleteQuestion }) => (
-  <Grid gap="10px">
-    {items.map((item, index) => (
-      <SortableItem
-        key={index}
-        index={index}
-        i={index}
-        value={item.value}
-        error={item.error}
-        updateQuestion={updateQuestion}
-        deleteQuestion={deleteQuestion}
+const SortableItem = SortableElement(
+  ({ value, error, i, updateQuestion, deleteQuestion }) => (
+    <Flex gridGap="10px" w="100%">
+      <DragHandle />
+      <Select
+        w="210px"
+        name="type"
+        value={value.type}
+        error={error.type}
+        onChange={(name, value) => updateQuestion(i, name, value)}
+        options={["Inclusion", "Exclusion"]}
       />
-    ))}
-  </Grid>
-));
+      <Input
+        placeholder="Question Prompt"
+        name="prompt"
+        value={value.prompt}
+        error={error.prompt}
+        onChange={(name, value) => updateQuestion(i, name, value)}
+      />
+      <IconButton
+        colorScheme=""
+        color="gray.500"
+        _hover={{ color: "red.500", bg: "red.100" }}
+        icon={<FaTrash />}
+        onClick={() => deleteQuestion(i)}
+      />
+    </Flex>
+  )
+);
+
+const SortableList = SortableContainer(
+  ({ items, updateQuestion, deleteQuestion }) => (
+    <Grid gap="10px">
+      {items.map((item, index) => (
+        <SortableItem
+          key={index}
+          index={index}
+          i={index}
+          value={item.value}
+          error={item.error}
+          updateQuestion={updateQuestion}
+          deleteQuestion={deleteQuestion}
+        />
+      ))}
+    </Grid>
+  )
+);
 
 export default ScreeningEdit;
