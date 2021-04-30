fdb1af7c49be9716dff7ce07cc837115d9c76b5f
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-04-30 03:27:21 -0400

    Refactored DescriptionAccessibilityScore

--- src/molecules/DescriptionAccessibilityScore.js
+++ src/molecules/DescriptionAccessibilityScore.js
@@ -6,20 +6,14 @@ import { FaInfoCircle } from "react-icons/fa";
 
 function DescriptionAccessibilityScore({ description }) {
   const getColor = (score) => {
-    if (isNaN(score)) {
-      return "gray";
-    }
-
-    if (score <= 20) {
-      return "red";
-    } else if (score >= 50) {
-      return "green";
-    } else {
-      return "yellow";
-    }
+    if (isNaN(score)) return "gray";
+    if (score <= 20) return "red";
+    if (score >= 50) return "green";
+    return "yellow";
   };
 
   const score = compute.readabilityIndex(description);
+  const value = score || 0;
   const color = getColor(score);
 
   return (
@@ -29,24 +23,18 @@ function DescriptionAccessibilityScore({ description }) {
           Description Accessibility Score
         </Heading>
         <Tag variant="solid" mr="8px" fontWeight="500" colorScheme={color}>
-          {isNaN(score) ? 0 : score}%
+          {value}%
         </Tag>
         <Tooltip label="This score is derived from the Flesch–Kincaid Readability Index. To improve your accessibility score, please use shorter sentences and words with fewer syllables.">
-          <Flex align="center">
+          <Box>
             <Icon color="gray.500" as={FaInfoCircle} />
-          </Flex>
+          </Box>
         </Tooltip>
       </Flex>
       <Text color="gray.500">
         Accessible study descriptions are shown to improve participant interest and enrollment rates
       </Text>
-      <Progress
-        value={isNaN(score) ? 0 : score}
-        size="md"
-        mt="8px"
-        bg="gray.200"
-        colorScheme={color}
-      />
+      <Progress value={value} mt="8px" bg="gray.200" colorScheme={color} />
     </Box>
   );
 }
