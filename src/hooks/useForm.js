748f5c746e73a0098a06ed3cfd01b359ff09986a
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-04-30 18:54:17 -0400

    Improved useForm hook

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,18 +1,61 @@
 import { useState, useEffect } from "react";
+import { useToast } from "@chakra-ui/react";
 
-function useForm(initial, checker, validate) {
-  const [inputs, setInputs] = useState(initial);
-  const [errors, setErrors] = useState(validate(initial));
+function useForm({ initial, validate, submit }) {
+  const toast = useToast();
+
+  const [inputs, setInputs] = useState({});
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const triggerErrorToast = () => {
+    toast({
+      title: "Connection Error!",
+      description:
+        "Your action could not be completed because of a connection error. Please try again later.",
+      status: "error",
+      duration: 5000,
+      isClosable: true,
+      position: "top",
+    });
+  };
+
+  const check = (name, value) => validate({ [name]: value })[name];
 
   const handleChange = (name, value) => {
-    setInputs({ ...inputs, [name]: value });
-    setErrors({ ...errors, [name]: checker(name, value) });
+    setInputs((prev) => ({ ...prev, [name]: value }));
+    setErrors((prev) => ({ ...prev, [name]: check(name, value) }));
+  };
+
+  const handleReset = () => {
+    const error = validate(initial);
+    const empty = Object.keys(initial).every((k) => !initial[k]);
+
+    setInputs(initial);
+    !empty && setErrors(error);
+  };
+
+  const handleSubmit = () => {
+    const error = validate(inputs);
+    const valid = Object.keys(error).every((k) => !error[k]);
+
+    if (!valid) {
+      setErrors(error);
+      setLoading(false);
+      return;
+    }
+
+    return submit(inputs)
+      .then(() => handleReset())
+      .catch(() => triggerErrorToast())
+      .finally(() => setLoading(false));
   };
 
   useEffect(() => {
-    setInputs(initial);
-    setErrors(validate(initial));
-  }, [initial]);
+    handleReset();
+  }, []);
 
-  return [inputs, errors, setInputs, setErrors, handleChange];
+  return { inputs, errors, loading, setInputs, setErrors, handleChange, handleReset, handleSubmit };
 }
+
+export default useForm;
