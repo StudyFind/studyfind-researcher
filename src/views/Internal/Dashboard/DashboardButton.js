06351538c6e4a29ffa232d3de371c50ce1dcc6a1
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-05-25 14:23:26 -0400

    Ensure tooltip is near hovered object

--- src/views/Internal/Dashboard/DashboardButton.js
+++ src/views/Internal/Dashboard/DashboardButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Link } from "components";
-import { Button, Tooltip } from "@chakra-ui/react";
+import { Box, Button, Tooltip } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa";
 
 function DashboardButton({ verified }) {
@@ -10,11 +10,13 @@ function DashboardButton({ verified }) {
       label={!verified && "You must verify your email before you can create any studies"}
       placement="bottom"
     >
-      <Link to="/fetch" isWrapper>
-        <Button isDisabled={!verified} leftIcon={<FaPlus />} colorScheme="blue">
-          Create Study
-        </Button>
-      </Link>
+      <Box>
+        <Link to="/fetch" isWrapper>
+          <Button isDisabled={!verified} leftIcon={<FaPlus />} colorScheme="blue">
+            Create Study
+          </Button>
+        </Link>
+      </Box>
     </Tooltip>
   );
 }
