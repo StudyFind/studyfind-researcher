8af789550b772bf66271e8ef044f6206a081ae3e
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-06-02 13:35:33 -0400

    Refactored Auth Blocks

--- src/views/External/Auth/Blocks/AuthForm.js
+++
@@ -1,9 +0,0 @@
-import { Form } from "components";
-import styled from "styled-components";
-
-export default styled(Form)`
-  width: 100%;
-  padding: 30px;
-  display: grid;
-  grid-gap: 15px;
-`;
--- src/views/External/Auth/Blocks/index.js
+++
@@ -1,7 +0,0 @@
-export { default as AuthForm } from "./AuthForm";
-export { default as AuthHeading } from "./AuthHeading";
-export { default as AuthName } from "./AuthName";
-export { default as AuthEmail } from "./AuthEmail";
-export { default as AuthPassword } from "./AuthPassword";
-export { default as AuthButton } from "./AuthButton";
-export { default as AuthTabLink } from "./AuthTabLink";
