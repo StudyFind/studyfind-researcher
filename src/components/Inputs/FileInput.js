cec9b0c23f57b2f4335912359586a910811b48d1
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-06-02 04:36:53 -0400

    Removed unneccessary prop that is native

--- src/components/Inputs/FileInput.js
+++ src/components/Inputs/FileInput.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Input, FormControl } from "@chakra-ui/react";
 import { Label, Error } from "./helpers";
 
-export const FileInput = ({ name, error, label, accept, onChange, ...rest }) => {
+export const FileInput = ({ name, error, label, onChange, ...rest }) => {
   const handleChange = (e) => {
     onChange(name, e.target.files[0]);
   };
@@ -15,7 +15,6 @@ export const FileInput = ({ name, error, label, accept, onChange, ...rest }) => 
         p="4px !important"
         w="100%"
         bg={error ? "red.100" : "white"}
-        accept={accept}
         onChange={handleChange}
         {...rest}
       />
