0bfa3c6daf03023fa268466677313416ae019e75
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-06-02 01:27:22 -0400

    Error helper component checked if string

--- src/components/Inputs/helpers/Error.js
+++ src/components/Inputs/helpers/Error.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import lodash from "lodash";
 import { FormErrorMessage } from "@chakra-ui/react";
 
 export const Error = ({ error, ...rest }) => {
-  return error ? <FormErrorMessage {...rest}>{error}</FormErrorMessage> : null;
+  return error && lodash.isString(error) && error.trim() ? (
+    <FormErrorMessage {...rest}>{error}</FormErrorMessage>
+  ) : null;
 };
