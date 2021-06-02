2187b3f591b386a15afe126b56a267db2f946819
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-06-02 13:34:24 -0400

    Refactored CreateStudy

--- src/views/Internal/Study/CreateStudy/Screening/Screening.js
+++ src/views/Internal/Study/CreateStudy/Screening/Screening.js
@@ -1,43 +1,12 @@
 import React from "react";
-import { useScreening } from "hooks";
 import ScreeningHead from "./ScreeningHead";
 import ScreeningGrid from "./ScreeningGrid";
 
 function Screening({ study, next, back }) {
-  const {
-    values,
-    errors,
-    loading,
-    createQuestion,
-    updateQuestion,
-    deleteQuestion,
-    clearQuestions,
-    resetQuestions,
-    sortQuestions,
-    handleSubmit,
-  } = useScreening(study);
-
-  const handleSubmitModified = () => {
-    handleSubmit().then(next);
-  };
-
   return (
     <>
       <ScreeningHead />
-      <ScreeningGrid
-        back={back}
-        values={values}
-        errors={errors}
-        loading={loading}
-        original={study.questions}
-        createQuestion={createQuestion}
-        updateQuestion={updateQuestion}
-        deleteQuestion={deleteQuestion}
-        resetQuestions={resetQuestions}
-        clearQuestions={clearQuestions}
-        sortQuestions={sortQuestions}
-        handleSubmit={handleSubmitModified}
-      />
+      <ScreeningGrid study={study} next={next} back={back} />
     </>
   );
 }
