8b0c101f066594fd69959c9b0604868c04f67d09
Vir Mittal <virmittal@gmail.com>
2021-08-14 19:50:34 +0530

    Cleaned up useStripeRole hook

--- src/hooks/useStripeRole.js
+++ src/hooks/useStripeRole.js
@@ -4,18 +4,15 @@ import { auth } from "database/firebase";
 function useStripeRole() {
 
   const[userStripeRole, setUserStripeRole] = useState()
-  const[loading, setLoading] = useState(true)
 
   const getCustomClaimRole = async () => {
     await auth.currentUser.getIdToken(true);
     const decodedToken = await auth.currentUser.getIdTokenResult();
     if (decodedToken && decodedToken.claims && decodedToken.claims.stripeRole){
       setUserStripeRole(decodedToken.claims.stripeRole);
-      setLoading(false);
     }
     else{
       setUserStripeRole('basic');
-      setLoading(false);
     }
   }
 
