fb9e1bc8b538735422d627c4cd3c5f620c5bb1d3
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-08-20 12:43:23 -0400

    Added updated tabs

--- src/pages/Internal/Study/CreateStudy/Review.js
+++ src/pages/Internal/Study/CreateStudy/Review.js
@@ -1,13 +1,12 @@
-import { useColor, useDetectDevice } from "hooks";
 import { auth } from "database/firebase";
-import { Box, Heading, Text, Tabs, TabList, TabItem, TabPanels, TabPanel } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 import StudyCardLarge from "components/feature/Study/StudyCard/StudyCardLarge";
 import LocationsList from "components/feature/Study/LocationsList/LocationsList";
+import HorizontalTabs from "components/complex/HorizontalTabs/HorizontalTabs";
+import QuestionsTable from "components/feature/Study/QuestionsTable/QuestionsTable";
 
 function Review({ newStudy }) {
-  const { isPhone } = useDetectDevice();
-
   const tabs = [
     {
       name: "Details",
@@ -26,10 +25,9 @@ function Review({ newStudy }) {
       ),
     },
     { name: "Locations", content: <LocationsList locations={newStudy.locations} /> },
+    { name: "Questions", content: <QuestionsTable questions={newStudy.questions} /> },
+    // { name: "Resources", content: <ResourcesList resources={newStudy.resources} /> },
   ];
-
-  const color = useColor("gray.400", "gray.500");
-  const borderColor = useColor("gray.300", "gray.600");
 
   return (
     <Box paddingY="20px">
@@ -38,29 +36,7 @@ function Review({ newStudy }) {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
       </Text>
-
-      <Tabs colorScheme="blue" height="100%">
-        <TabList
-          overflowX={isPhone && "scroll"}
-          overflowY={isPhone && "hidden"}
-          borderBottomWidth={isPhone ? "0" : "2px"}
-          borderBottomColor={borderColor}
-          height="42px"
-        >
-          {tabs.map((t, i) => (
-            <TabItem key={i} color={color} borderBottomColor={borderColor}>
-              {t.name}
-            </TabItem>
-          ))}
-        </TabList>
-        <TabPanels>
-          {tabs.map((t, i) => (
-            <TabPanel key={i} olor={color} borderBottomColor={borderColor}>
-              {t.content}
-            </TabPanel>
-          ))}
-        </TabPanels>
-      </Tabs>
+      <HorizontalTabs tabs={tabs} useURL={false} paddingY="20px" />
     </Box>
   );
 }
