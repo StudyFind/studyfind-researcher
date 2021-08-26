5872f8ddb2a9252ac0924e1572bbf7dc521970e8
ivanyauu <yauivan1122@gmail.com>
2021-08-26 12:32:41 -0700

    Made scheduling a premium feature, may need to style it in the future.

--- src/views/Internal/Schedule/Schedule.js
+++ src/views/Internal/Schedule/Schedule.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { StripeContext } from "context";
 import moment from "moment";
 import { datetime } from "functions";
 import { Box, Grid, Heading, Text } from "@chakra-ui/react";
@@ -9,16 +10,28 @@ function Schedule() {
   const today = moment().format("YYYY-MM-DD");
   const [date, setDate] = useState(today);
 
+  const userStripeRole = useContext(StripeContext);
+
   const displayDate = datetime.getFriendlyDate(date);
 
   return (
+    
     <Grid gap="50px" templateColumns="1fr 300px">
-      <Box>
-        <Heading size="lg">Meetings</Heading>
-        <Text color="gray.500">{displayDate}</Text>
-        <Meetings date={date} />
-      </Box>
-      <Calendar today={today} date={date} setDate={setDate} />
+        {(userStripeRole === 'premium') ? (
+        <>
+          <Box>
+            <Heading size="lg">Meetings</Heading>
+            <Text color="gray.500">{displayDate}</Text>
+            <Meetings date={date} />
+          </Box>
+          <Calendar today={today} date={date} setDate={setDate} />
+        </>
+        ) : (
+        <Box>
+          <Heading size="lg">Scheduling is a Premium Feature!</Heading>
+          <Text color="blue.500"><a href="/pricing">Click Here to Subscribe for Premium</a></Text>
+        </Box>
+        )}
     </Grid>
   );
 }
