66cbf0b589ffbf27c06fdd9f4dc9d5ae98130df4
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-08-26 00:13:54 -0400

    ESLint warning fixed

--- src/pages/Internal/Study/ViewStudy/Participants/Participants.js
+++ src/pages/Internal/Study/ViewStudy/Participants/Participants.js
@@ -1,13 +1,13 @@
 import { useHistory } from "react-router-dom";
 import { useParticipantQueryWithFilters, usePathParams } from "hooks";
 
-import { Button } from "@chakra-ui/react";
+// import { Button } from "@chakra-ui/react";
 import { Loader } from "components";
 
 import TabHeader from "../TabHeader";
 
 import ParticipantsList from "./ParticipantsList";
-import ParticipantsFilter from "./ParticipantsFilter";
+// import ParticipantsFilter from "./ParticipantsFilter";
 import ParticipantsDrawer from "./ParticipantsDrawer";
 import ParticipantsEmpty from "./ParticipantsEmpty";
 
