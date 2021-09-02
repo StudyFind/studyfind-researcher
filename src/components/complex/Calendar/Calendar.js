34e207bc6fa5a98b6f158c3f6fa661afc1f763a7
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-02 15:56:40 -0700

    Calendar Refactor

--- src/components/complex/Calendar/Calendar.js
+++ src/components/complex/Calendar/Calendar.js
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
 import moment from "moment";
+
+import { useState, useEffect } from "react";
 
 import { Grid, Divider } from "@chakra-ui/react";
 
--- src/components/complex/Calendar/CalendarHeadButton.js
+++ src/components/complex/Calendar/CalendarHeadButton.js
@@ -1,19 +1,13 @@
-import { IconButton } from "@chakra-ui/react";
 import { useColor } from "hooks";
+
+import { IconButton } from "@chakra-ui/react";
 
 function CalendarHeadButton({ icon, onClick }) {
   const color = useColor("blue.500", "blue.200");
   const background = useColor("blue.100", "rgba(144, 205, 244, 0.16)");
 
   return (
-    <IconButton
-      colorScheme=""
-      color={color}
-      background={background}
-      size="xs"
-      icon={icon}
-      onClick={onClick}
-    />
+    <IconButton color={color} background={background} size="xs" icon={icon} onClick={onClick} />
   );
 }
 
