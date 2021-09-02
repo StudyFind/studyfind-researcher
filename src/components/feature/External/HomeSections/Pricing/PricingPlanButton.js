d8f9bbf4fdae31d6348aec2d8b24812c72a82e67
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-02 15:13:28 -0700

    Made Pricing Plan Button link to /auth

--- src/components/feature/External/HomeSections/Pricing/PricingPlanButton.js
+++ src/components/feature/External/HomeSections/Pricing/PricingPlanButton.js
@@ -1,8 +1,13 @@
+import { useHistory } from "hooks";
+
 import { Button } from "@chakra-ui/react";
 
 function PricingPlanButton() {
+  const history = useHistory();
+
   const handleClick = () => {
     localStorage.setItem("redirect", "/account/subscription");
+    history.push("/auth");
   };
 
   return (
