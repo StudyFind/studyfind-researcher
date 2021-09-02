9ad7c1b0d638c32cf53869a9d78c66d3c03e9959
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-02 15:14:15 -0700

    Added link to home from team page

--- src/pages/External/Team/Team.js
+++ src/pages/External/Team/Team.js
@@ -1,5 +1,8 @@
 import { useColor } from "hooks";
-import { Box, Divider } from "@chakra-ui/react";
+
+import { Box, Icon, Text, Divider, HStack } from "@chakra-ui/react";
+import { Link } from "components";
+import { FaChevronLeft } from "react-icons/fa";
 
 import Interns from "./Interns";
 import Board from "./Board";
@@ -10,6 +13,18 @@ function Team() {
 
   return (
     <Box>
+      <HStack justify="flex-start" padding="30px">
+        <Link to="/">
+          <HStack
+            spacing="5px"
+            align="center"
+            color="blue.500"
+            _hover={{ textDecoration: "underline" }}
+          >
+            <Icon as={FaChevronLeft} fontSize="12px" /> <Text>Return home</Text>
+          </HStack>
+        </Link>
+      </HStack>
       <Interns />
       <Divider borderColor={dividerColor} />
       <Board />
