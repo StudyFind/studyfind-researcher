836eddf365c7b97219ab32e8cceeb0615d4f825a
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-14 19:42:25 -0400

    Updated Page

--- src/components/simple/Page.js
+++ src/components/simple/Page.js
@@ -1,13 +1,19 @@
-import { useColor } from "hooks";
+import { useColor, useDetectDevice } from "hooks";
 import { Box } from "@chakra-ui/react";
 import { Loader } from "./Loader";
 
 export const Page = ({ children, isLoading, ...rest }) => {
+  const { isPhone } = useDetectDevice();
+
   const background = useColor("#f8f9fa", "gray.800");
 
   return (
-    <Box background={background} {...rest}>
-      {isLoading ? <Loader height="calc(100vh - 80px)" /> : children}
+    <Box background={background} padding={isPhone ? "20px" : "40px"} {...rest}>
+      {isLoading ? (
+        <Loader height={`calc(100vh - ${isPhone ? 40 : 80}px)`} />
+      ) : (
+        children
+      )}
     </Box>
   );
 };
