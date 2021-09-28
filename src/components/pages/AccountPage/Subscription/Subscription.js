31e73fa87b1bcdf2fc754244150f734aece1725f
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-27 20:34:23 -0400

    Subscription changes

--- src/components/pages/AccountPage/Subscription/Subscription.js
+++ src/components/pages/AccountPage/Subscription/Subscription.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+import { useTriggerToast, usePathParams, useCollection } from "hooks";
+import { useHistory } from "react-router-dom";
 
 import { auth, firestore, functions } from "database/firebase";
+import { toasts } from "templates";
 
 import { Grid } from "@chakra-ui/react";
 import { Loader } from "components";
@@ -16,21 +19,35 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
   const [isBilledAnnually, setIsBilledAnnually] = useState(true);
   const [selectedPlan, setSelectedPlan] = useState("basic");
   const [currentPlan, setCurrentPlan] = useState("");
+  const [planItems, setPlanItems] = useState({});
   const [loading, setLoading] = useState(true);
   const [linking, setLinking] = useState(false);
 
+  const triggerToast = useTriggerToast();
+
+  const history = useHistory();
+
+  const { action } = usePathParams();
+
   const selectedPlanID = {
     basic: {
-      annually: "price_1JU1hsIzlngCzbHLkjD7vwaj",
-      monthly: "price_1JU1gaIzlngCzbHLv2NowTqn",
+      // annually: "price_1JU1hsIzlngCzbHLkjD7vwaj", UNCOMMENT WHEN MERGING TO PRODUCTION
+      annually: "price_1JboRyIzlngCzbHLrjB591Rl",
+      // old_monthly: "price_1JU1gaIzlngCzbHLv2NowTqn",
+      monthly: "price_1JeTbpIzlngCzbHLBw68zzbH", //cheap monthly
+      // test_monthly: "price_1Jbv5aIzlngCzbHLzcyoqZRu",
     },
     standard: {
-      annually: "price_1JU1jwIzlngCzbHL32su7mlE",
-      monthly: "price_1JU1jQIzlngCzbHLwgm0SVAe",
+      // annually: "price_1JU1jwIzlngCzbHL32su7mlE", SAME
+      annually: "price_1JboXRIzlngCzbHLp3oycIFU",
+      // monthly: "price_1JU1jQIzlngCzbHLwgm0SVAe",
+      monthly: "price_1Jbv6KIzlngCzbHLIWwhVEcx",
     },
     premium: {
-      annually: "price_1JU1n4IzlngCzbHLAFMoPmtq",
-      monthly: "price_1JU1mJIzlngCzbHLsYWJCSFm",
+      // annually: "price_1JU1n4IzlngCzbHLAFMoPmtq", SAME
+      annually: "price_1JboY8IzlngCzbHLYG7R09VN",
+      // monthly: "price_1JU1mJIzlngCzbHLsYWJCSFm",
+      monthly: "price_1Jbv6vIzlngCzbHLkn8dctTn",
     },
   }[selectedPlan][isBilledAnnually ? "annually" : "monthly"];
 
@@ -53,8 +70,8 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
         price: selectedPlanID,
         trial_from_plan: true,
         allow_promotion_codes: true,
-        success_url: window.location.origin,
-        cancel_url: window.location.origin,
+        success_url: window.location.origin + "/account/subscription/success",
+        cancel_url: window.location.origin + "/account/subscription/cancel",
       });
 
     docRef.onSnapshot((snapshot) => {
@@ -62,6 +79,7 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
 
       if (error) {
         setLinking(false);
+        triggerToast(toasts.stripeError(error.message));
       }
 
       if (url) {
@@ -85,10 +103,32 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
   const getCustomClaimRole = async () => {
     await auth.currentUser.getIdToken(true);
     const decodedToken = await auth.currentUser.getIdTokenResult();
-    const plan = decodedToken?.claims?.stripeRole || "basic";
+    const plan = decodedToken?.claims?.stripeRole;
+    if (plan) {
+      const docRef = firestore
+        .collection("researchers")
+        .doc(auth.currentUser.uid)
+        .collection("subscriptions");
 
-    setCurrentPlan(plan);
-    setSelectedPlan(plan);
+      docRef.onSnapshot((snapshot) => {
+        /*
+        ASSUMING ONLY 1 SUBSCRIPTION PER RESEARCHER, MAY NEED TO UPDATE TO ACCOUNT FOR MULTIPLE SUBSCRIPTION TYPES
+        */
+        snapshot.forEach((subscription) => {
+          const { items, current_period_end, cancel_at_period_end } =
+            subscription.data();
+          const { amount } = items[0].plan;
+          setPlanItems({
+            amount,
+            current_period_end: current_period_end.toDate(),
+            cancel_at_period_end,
+          });
+        });
+      });
+    }
+
+    setCurrentPlan(plan || "free");
+    setSelectedPlan(plan || "basic");
     setLoading(false);
   };
 
@@ -96,13 +136,31 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
     getCustomClaimRole();
   }, []);
 
+  useEffect(() => {
+    if (action) {
+      if (action === "cancel") {
+        triggerToast(toasts.stripeCancel);
+      }
+
+      if (action === "success") {
+        triggerToast(toasts.stripeSuccess);
+      }
+
+      history.push("/account/subscription");
+    }
+  }, [action]);
+
   const plans = [
     {
       icon: SiMicrosoft,
       name: "basic",
       title: "Basic",
       price: ["$29", "$19"],
-      features: ["Create Studies", "Recruit Participants", "Track Participant Status"],
+      features: [
+        "Create Studies",
+        "Recruit Participants",
+        "Track Participant Status",
+      ],
     },
 
     {
@@ -110,7 +168,11 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
       name: "standard",
       title: "Standard",
       price: ["$99", "$79"],
-      features: ["Everything in Basic", "Participant Reminders", "Schedule Meetings"],
+      features: [
+        "Everything in Basic",
+        "Participant Reminders",
+        "Schedule Meetings",
+      ],
       isPopular: true,
     },
 
@@ -119,7 +181,11 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
       name: "premium",
       title: "Premium",
       price: ["$249", "$199"],
-      features: ["Everything in Standard", "Instant Messaging", "Email and Text Notifications"],
+      features: [
+        "Everything in Standard",
+        "Instant Messaging",
+        "Email and Text Notifications",
+      ],
     },
   ];
 
@@ -136,9 +202,12 @@ function Subscription({ showButtons, handleCancel, handleUpdate }) {
         />
         {loading ? (
           <Loader height="300px" />
-        ) : currentPlan === "Premium" ? (
+        ) : currentPlan !== "free" ? (
           <SubscriptionView
+            plans={plans}
+            isBilledAnnually={isBilledAnnually}
             currentPlan={currentPlan}
+            planItems={planItems}
             linking={linking}
             handleManageSubscription={handleManageSubscription}
           />
