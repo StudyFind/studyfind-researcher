369e426b1c8360c0e4af03af1df2885ead54a6d3
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-30 14:07:55 -0400

    Changed dashboard empty wording

--- src/pages/Internal/Dashboard/DashboardEmpty.js
+++ src/pages/Internal/Dashboard/DashboardEmpty.js
@@ -8,8 +8,9 @@ function DashboardEmpty({ verified }) {
       <Box width="500px">
         <Heading size="lg">Create your first study</Heading>
         <Text color="gray.500" marginTop="8px" marginBottom="30px">
-          You can add your study using its Clinical Trials ID and begin recruiting and managing
-          participants almost instantaneously. StudyFind automates a lot of your work for you.
+          You can create a new study and begin recruiting and managing
+          participants almost instantaneously. Follow the study creation steps
+          carefully to yeild the best outcomes from StudyFind.
         </Text>
         <DashboardButton verified={verified} />
       </Box>
