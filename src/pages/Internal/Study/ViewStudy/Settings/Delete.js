c7d02e6a4dfdafa3737502ce6c320e2b4fc03424
Yohan Jhaveri <yohanjhaveri@gmail.com>
2021-09-29 23:43:40 -0400

    Fixed crash

--- src/pages/Internal/Study/ViewStudy/Settings/Delete.js
+++ src/pages/Internal/Study/ViewStudy/Settings/Delete.js
@@ -20,22 +20,18 @@ function Delete({ study }) {
   };
 
   const handleConfirm = () => {
+    history.push("/");
     return researchStudy
       .delete(study.id)
-      .then(() => {
-        history.push("/");
-        triggerToast(toasts.deletedStudy);
-      })
-      .catch(() => {
-        triggerToast(toasts.connectionError);
-      });
+      .then(() => triggerToast(toasts.deletedStudy))
+      .catch(() => triggerToast(toasts.connectionError));
   };
 
   const handleDelete = () => {
     if (studyID === study.id) {
       triggerConfirm({
         title: "Delete Study",
-        description: `Are you sure you want to delete study with study id "${studyID}"?`,
+        description: `Are you sure you want to delete study with ID "${studyID}"?`,
         color: "red",
         button: "Delete",
         handleConfirm,
@@ -48,8 +44,8 @@ function Delete({ study }) {
       <Heading size="md">Delete Study</Heading>
 
       <Text my="8px" color="gray.500">
-        Deleting your study will erase all data associated with your research study. This is a
-        permanant action and cannot be undone.
+        Deleting your study will erase all data associated with your research
+        study. This is a permanant action and cannot be undone.
       </Text>
 
       <Text mt="24px" mb="8px" color="gray.500">
@@ -58,10 +54,23 @@ function Delete({ study }) {
 
       <Form onSubmit={handleDelete}>
         <Grid gap="10px" maxW="240px">
-          <TextInput value={studyID} placeholder="Type here..." onChange={handleChange} />
-          <Tooltip label={studyID !== study.id && "Entered ID does not match the study ID"}>
+          <TextInput
+            value={studyID}
+            placeholder="Type here..."
+            onChange={handleChange}
+          />
+          <Tooltip
+            label={
+              studyID !== study.id && "Entered ID does not match the study ID"
+            }
+          >
             <Box>
-              <Button type="submit" w="100%" colorScheme="red" isDisabled={studyID !== study.id}>
+              <Button
+                type="submit"
+                w="100%"
+                colorScheme="red"
+                isDisabled={studyID !== study.id}
+              >
                 Delete
               </Button>
             </Box>
